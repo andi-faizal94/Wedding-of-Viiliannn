33215120b3386844d79d4e2be36b0f33a7baa508
Andi Faizal <andifaizal647@gmail.com>
2022-05-12 19:35:26 +0700

    add bg

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 function App() {
   return (
-    <div className="App bg-black">
+    <div className="App bg-[#0a192f]">
       <h1 className="p-4 text-3xl text-center font-lato font-extrabold text-transparent bg-clip-text bg-gradient-to-br from-pink-100 to-red-900 mb-4">
-        Wedding Erfi & Frendi
+        Wedding
       </h1>
+      <h1 className="text-3xl text-center font-lato font-extrabold text-transparent bg-clip-text bg-gradient-to-br from-pink-100 to-red-900 mb-4">
+        Erfi & Frendi
+      </h1>
+
       <iframe
         className="w-full min-h-screen p-2 aspect-square rounded-md"
         src="https://www.youtube.com/embed/DYlDtDjGEI8"
