0772e0094bf9a4cbbe32fbe1bbd568a72a6fa7e1
Andi Faizal <andifaizal647@gmail.com>
2022-05-12 19:27:00 +0700

    adjust wedding

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 function App() {
   return (
-    <div className="App aspect-w-16 aspect-h-9">
-      <h3 className="text-center">Wedding Ervi</h3>
+    <div className="App bg-black">
+      <h1 className="p-4 text-3xl text-center font-lato font-extrabold text-transparent bg-clip-text bg-gradient-to-br from-pink-100 to-red-900 mb-4">
+        Wedding Erfi & Frendi
+      </h1>
       <iframe
-        className="w-full min-h-screen aspect-square rounded-md"
+        className="w-full min-h-screen p-2 aspect-square rounded-md"
         src="https://www.youtube.com/embed/DYlDtDjGEI8"
         frameBorder="0"
         allow="autoplay; encrypted-media"
