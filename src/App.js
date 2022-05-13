f857ef07bd9a5a994085e195efc9bd0843b96ea6
Andi Faizal <andifaizal647@gmail.com>
2022-05-13 14:45:59 +0700

    add animation

--- src/App.js
+++ src/App.js
@@ -1,10 +1,51 @@
+import { useState, useEffect } from 'react';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+
+const delay = 1;
+
 function App() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    AOS.init({
+      duration: 2000,
+    });
+  }, []);
+
+  useEffect(() => {
+    let timer1 = setTimeout(() => setLoading(false), delay * 6000);
+    return () => {
+      clearTimeout(timer1);
+    };
+  }, []);
+
+  if (loading)
+    return (
+      <div className="flex flex-col justify-center items-center main">
+        <div className="loader-wrapper">
+          <div className="loader">
+            <div className="loader loader-inner"></div>
+          </div>
+        </div>
+        <h1 className="p text-center text-success" data-text="Loading ..">
+          Loading ...
+        </h1>
+      </div>
+    );
+
   return (
-    <div className="App bg-[#0a192f]">
-      <h1 className="p-4 text-3xl text-center font-lato font-extrabold text-transparent bg-clip-text bg-gradient-to-br from-pink-100 to-red-900 mb-4">
+    <div className="App bg-[#0a192f]" data-aos="fade-down">
+      <h1
+        className="p-4 text-3xl text-center font-lato font-extrabold text-transparent bg-clip-text bg-gradient-to-br from-pink-100 to-red-900 mb-4"
+        data-aos="fade-down"
+      >
         Wedding
       </h1>
-      <h1 className="text-3xl text-center font-lato font-extrabold text-transparent bg-clip-text bg-gradient-to-br from-pink-100 to-red-900 mb-4">
+      <h1
+        className="text-3xl text-center font-lato font-extrabold text-transparent bg-clip-text bg-gradient-to-br from-pink-100 to-red-900 mb-4"
+        data-aos="zoom-in-up"
+      >
         Erfi & Frendi
       </h1>
 
@@ -15,15 +56,15 @@ function App() {
         allow="autoplay; encrypted-media"
         allowFullScreen
         title="video"
+        data-aos="fade-up"
       ></iframe>
 
       <footer className="text-white font-lato text-center p-4 cursor-pointer">
-        Made by
         <a
-          className="text-md ml-1 font-lato"
+          className="text-md font-lato"
           href="https://andi-3ujiqdvp9-andi-faizal94.vercel.app/"
         >
-          Andi
+          Made by Andi
         </a>
       </footer>
     </div>
